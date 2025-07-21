b5cdf3cb98f8dd2feaf87df2223e75140af1946f
Ken050493 <abc93050406@gmail.com>
2025-07-22 01:43:07 +0800

    Update powerset.cpp

--- hw1/src/powerset.cpp
+++ hw1/src/powerset.cpp
@@ -3,31 +3,29 @@
 using namespace std;
 
 const int MAX_SIZE = 10;
-
-void generatePowerset(string set[], string current[], int index, int currentSize, int setSize) 
+void generatePowerset(string set[], string current[], int index, int currentSize, int setSize)
 {
-    if (index == setSize) {
+    if (index == setSize)
+    {
         cout << "{ ";
-        for (int i = 0; i < currentSize; ++i) {
+        for (int i = 0; i < currentSize; ++i)
+        {
             cout << current[i] << " ";
         }
         cout << "}" << endl;
         return;
     }
-
     generatePowerset(set, current, index + 1, currentSize, setSize);
-
     current[currentSize] = set[index];
     generatePowerset(set, current, index + 1, currentSize + 1, setSize);
 }
 
-int main() {
-    string set[MAX_SIZE] = {"a", "b", "c"};
+int main()
+{
+    string S[MAX_SIZE] = {"a", "b", "c"};
     string current[MAX_SIZE];
     int setSize = 3;
-
     cout << "Powerset of {a, b, c}:" << endl;
-    generatePowerset(set, current, 0, 0, setSize);
-
+    generatePowerset(S, current, 0, 0, setSize);
     return 0;
 }
