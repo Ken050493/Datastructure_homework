2984df862dd8d50cead1c94788065abc8c9e0e8e
Ken050493 <220832515+Ken050493@users.noreply.github.com>
2025-07-21 18:03:49 +0000

    style: auto format using clang-format

--- hw1/src/powerset.cpp
+++ hw1/src/powerset.cpp
@@ -3,30 +3,26 @@
 using namespace std;
 
 const int MAX_SIZE = 10;
-void generatePowerset(string set[], string current[], int index, int currentSize, int setSize)
-{
-    if (index == setSize)
-    {
-        cout << "{ ";
-        for (int i = 0; i < currentSize; ++i)
-        {
-            cout << current[i] << " ";
-        }
-        cout << "}" << endl;
-        return;
+void generatePowerset(string set[], string current[], int index,
+                      int currentSize, int setSize) {
+  if (index == setSize) {
+    cout << "{ ";
+    for (int i = 0; i < currentSize; ++i) {
+      cout << current[i] << " ";
     }
-    generatePowerset(set, current, index + 1, currentSize, setSize);
-    current[currentSize] = set[index];
-    generatePowerset(set,          
-        current, index + 1, currentSize + 1, setSize);
+    cout << "}" << endl;
+    return;
+  }
+  generatePowerset(set, current, index + 1, currentSize, setSize);
+  current[currentSize] = set[index];
+  generatePowerset(set, current, index + 1, currentSize + 1, setSize);
 }
 
-int main()
-{
-    string S[MAX_SIZE] = {"a", "b", "c"};
-    string current[MAX_SIZE];
-    int setSize = 3;
-    cout << "Powerset of {a, b, c}:" << endl;
-    generatePowerset(S, current, 0, 0, setSize);
-    return 0;
+int main() {
+  string S[MAX_SIZE] = {"a", "b", "c"};
+  string current[MAX_SIZE];
+  int setSize = 3;
+  cout << "Powerset of {a, b, c}:" << endl;
+  generatePowerset(S, current, 0, 0, setSize);
+  return 0;
 }
